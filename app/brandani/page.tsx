46d398af37f47144e3ca8ab9e4b1e1717451db3e
Eddiesantle <wedleys8@gmail.com>
2024-09-19 14:05:35 -0300

    REFACTOR: eslint button correção

--- app/brandani/page.tsx
+++ app/brandani/page.tsx
@@ -2,7 +2,7 @@
 'use client'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
